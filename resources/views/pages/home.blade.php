7a833c63c0a8e71fd1dcbd9e269dc12198d7fa63
unknown <chadiegilaugis@gmail.com>
2021-11-13 09:45:37 +0800

    bladed

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+    <div class="container d-flex align-items-center justify-content-center mt-5">
+        <h1 class="text-center">Home Page</h1>
+    </div>
+
+@endsection
